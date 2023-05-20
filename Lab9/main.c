aa5373afd57d57ea7ab0ddc331aa4a44ee706daa
Keshabl <nochvin1993@mail.ru>
2023-05-20 11:07:08 +1000

    fg

--- Lab9/main.c
+++ Lab9/main.c
@@ -2,72 +2,91 @@
 #include <string.h>
 
 int main() {
-    char input[100]; // строка для ввода выражения
-    char output[100] = ""; // строка для вывода результата
-    char *token; // указатель на текущий токен
-
-    printf("Введите выражение: ");
-    fgets(input, 100, stdin); // считываем строку с консоли
-
-    token = strtok(input, "+- "); // получаем первый токен
-
-    // добавляем первый токен в строку вывода
-    strcat(output, token);
-
-    while (token != NULL) {
-        token = strtok(NULL, "+- "); // получаем следующий токен
-
-        if (token != NULL) {
-            // если токен - оператор "+", "-", то добавляем пробел и сам оператор с пробелами
-            if (strcmp(token, "+") == 0) {
-                strcat(output, " + ");
-            } else if (strcmp(token, "-") == 0) {
-                strcat(output, " - ");
+    char expression[100];
+    printf("Insert: ");
+    fgets(expression, 100, stdin);
+    char new_expression[100] = "";
+    int j = 0;
+    int i = 0;
+    
+    for(i = 0; i < strlen(expression); i++) {
+        if(expression[i] == '+') {
+            if(i > 0 && expression[i-1] != ' ' && expression[i-1] != '+') {
+                new_expression[j++] = ' ';
             }
-            // иначе добавляем пробел и операнд
-            else {
-                strcat(output, " ");
-                strcat(output, token);
+            new_expression[j++] = '+';
+            if(i < strlen(expression)-1 && expression[i+1] != ' ' && expression[i+1] != '+') {
+                new_expression[j++] = ' ';
             }
+        } else {
+            new_expression[j++] = expression[i];
         }
     }
 
-    printf("Результат: %s\n", output);
-
-
-	
-	
-	
+	char input[100] = "";
+	j = 0;
+    i = 0;
+    
+    for(i = 0; i < strlen(new_expression); i++) {
+        if(new_expression[i] == '-') {
+            if(i > 0 && new_expression[i-1] != ' ' && new_expression[i-1] != '-') {
+                input[j++] = ' ';
+            }
+            input[j++] = '-';
+            if(i < strlen(new_expression)-1 && new_expression[i+1] != ' ' && new_expression[i+1] != '-') {
+                input[j++] = ' ';
+            }
+        } else {
+            input[j++] = new_expression[i];
+        }
+    }
+    
+	if(input[0] == '-' || input[0] == '+') {
+		i = 0;
+    	for (i=strlen(input)-1;i>=0;i--)
+    		input[i+1] = input[i];
+		input[0]=' ';	
+		i = 0;
+    	for (i=strlen(input)-1;i>=0;i--)
+    		input[i+1] = input[i];
+		input[0]='0';	
+		}	
+		
+	printf("Primer: %s\n", input);
 	
 	/*
-    double result = 0; // результат вычислений
-    double operand; // текущий операнд
-
-    token = strtok(output, " "); // получаем первый токен
-
-    // инициализируем результат первым операндом
-    sscanf(token, "%lf", &result);
+		-125.6 +2 -2+4 - 2+ 2 +0.1 -2-6.7
+	*/
+	
+	char *token;
+    double result = 0;
+    double operand;
+	
+ 	token = strtok(input, " "); 
+	sscanf(token, "%lf", &result);
 
     while (token != NULL) {
-        token = strtok(NULL, " "); // получаем следующий токен
+        token = strtok(NULL, " "); 
 
         if (token != NULL) {
-            // если токен - оператор "+", то выполняем сложение
+
             if (strcmp(token, "+") == 0) {
-                token = strtok(NULL, " "); // получаем следующий токен
+                token = strtok(NULL, " "); 
                 sscanf(token, "%lf", &operand);
                 result += operand;
             }
-            // если токен - оператор "-", то выполняем вычитание
+          
             else if (strcmp(token, "-") == 0) {
-                token = strtok(NULL, " "); // получаем следующий токен
+                token = strtok(NULL, " ");
                 sscanf(token, "%lf", &operand);
                 result -= operand;
             }
+
         }
     }
 
-    printf("Result: %lf\n", result);
-*/
+    printf("Answer: %lf\n", result);
+
     return 0;
 }
+
